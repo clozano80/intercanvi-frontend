1a608d931931707a32e0682a64f188d99d0bd70a
clozano80 <50866976+clozano80@users.noreply.github.com>
2022-07-06 13:41:05 +0200

    code: app - test4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Intercanvi test. 
-          Second test.
-          Tercer test.
-        </p>
+        <p>Intercanvi test. Second test. Tercer test. Cuarto test.</p>
       </header>
     </div>
   );
