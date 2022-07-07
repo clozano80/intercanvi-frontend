5e648d15578352e6868005f3d781c282a6c7dc1b
clozano80 <50866976+clozano80@users.noreply.github.com>
2022-07-07 10:07:23 +0200

    code: app  - texts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Intercanvi test. Second test. Tercer test. Cuarto test.</p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Intercanvi test. Second test. Tercer test. Cuarto test.</p>
+				<p>public</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
